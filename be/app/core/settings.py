6365927b0421be0bb75fe4c4ca9886df18717ae4
parksehun <davidpark8575@gmail.com>
2025-09-19 01:21:10 +0900

    project reset

--- be/app/core/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-APP_NAME = os.environ.get("APP_NAME", "you_are_volante")
-APP_ENV = os.environ.get("APP_ENV", "local")
-CORS_ORIGINS = os.environ.get("CORS_ORIGINS", "*").split(",")  # "*" 또는 "http://localhost:5173"
-LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
--- be/app/routers/api_v1.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.routers import teams
-
-api_v1 = APIRouter(prefix="/api/v1")
-
-api_v1.include_router(teams.router)
