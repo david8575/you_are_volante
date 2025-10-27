6178eab9db3c8da83ae6c2575a0a0d16a558d538
parksehun <davidpark8575@gmail.com>
2025-10-28 00:18:51 +0900

    feat: team app 생성 및  model wpwkr

--- accounts/models.py
+++ accounts/models.py
@@ -16,8 +16,6 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
-    # 아바타 추후 추가 -> Pillow 이용 예정 
-    # avartar = models.ImageField(upload_to="avartars/", blank=True, null=True) 
 
     def __str__(self):
         return f"Profile({self.user.username})"
---
+++ teams/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import Team
+# Register your models here.
