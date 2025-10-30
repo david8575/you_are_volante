254b1f1e9e5c140dbebbad4c0c652e05cc3762e4
parksehun <davidpark8575@gmail.com>
2025-10-30 15:55:34 +0900

    feat: accounts urls 제작

---
+++ backend/_project/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = 'accounts'
+
+url_patterns = [
+    path('register/', RegisterView.as_view(), name='register'), # 회원가입
+    path('login/', LoginView.as_view(), name='login'), # 로그인
+    path('logout/', LogoutView.as_view(), name='logout'), # 로그아웃
+    path('me/', UserDetailView.as_view(), name='user-detail'), # 내정보 조회
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'), # 비밀번호 변경
+]
+
